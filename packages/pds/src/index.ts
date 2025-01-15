caee0743b8edf5b51f1d7c31f77655830a885e0d
Reto <rethab@protonmail.ch>
2025-01-15 07:37:12 +0100

    chore: include path

--- packages/pds/src/index.ts
+++ packages/pds/src/index.ts
@@ -95,7 +95,7 @@ export class PDS {
 
     server = API(server, ctx)
 
-    const metricsMiddleware = promBundle({ includeMethod: true })
+    const metricsMiddleware = promBundle({ includeMethod: true, includePath: true })
 
     const app = express()
     app.set('trust proxy', true)
